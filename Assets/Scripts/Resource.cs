63808282fd80f6f6517fbcea854b9b347c782ec6
Milan Koerner-Safrata <milan@scrnprnt.ca>
2021-09-22 10:15:24 -0700

    arranging collage in order of appearnace

---
+++ Assets/Scripts/Resource.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class Resource
+{
+    public float amount;
+
+    public UnityEvent OnWholeNumberChanged;
+
+    public void Increment(){
+        amount++;
+        OnWholeNumberChanged.Invoke();
+    }
+
+    public void ChangeValue(float diff){
+
+    }
+}
