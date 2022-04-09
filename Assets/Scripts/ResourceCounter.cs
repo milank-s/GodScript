d781fa48cd6eb321e45f5095610cade7f63a4093
Milan Koerner-Safrata <milan@scrnprnt.ca>
2022-04-09 12:10:05 -0700

    continuing to change how resource counters manage themselves

--- Assets/Scripts/ResourceCounter.cs
+++ Assets/Scripts/ResourceCounter.cs
@@ -7,19 +7,8 @@ public class ResourceCounter : Counter
     public ResourceType resourceType;
     Resource r;
     public void Awake(){
-        title.SetText(resourceType.ToString());
+        //title.SetText(resourceType.ToString());
         r = Resources.GetResource(resourceType);
         r.OnWholeNumberChanged += SetAmount;
-    }
-
-    public override IEnumerator Reveal(float time = 1){
-        
-        yield return StartCoroutine(title.Reveal(1));
-        yield return new WaitForSeconds(0.25f);
-        yield return StartCoroutine(base.Reveal(1));
-    }
-    public override void SetText(string t){
-        float amount = float.Parse(t);
-        text.text = amount.ToString("F0");
     }
 }
